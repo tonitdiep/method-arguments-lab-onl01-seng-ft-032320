e01d6c297f4f14c2e46e44f7026cce835424dfaf
Toni  Diep <adiep123@gmail.com>
2020-03-24 02:54:04 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,3 +6,7 @@ end
 def introduction(name)
     puts "Hi, my name is #{{name}."
 end
+
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to to program in #{language}."
+end
